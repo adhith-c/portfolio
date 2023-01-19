5fb7eeac8d4baa38427df09d6229841db38529cb
adhith-c <adhithadhi17@gmail.com>
2023-01-19 18:07:54 +0530

    cv updated

--- js/main.js
+++ js/main.js
@@ -117,13 +117,10 @@
 
 //validations
  
-function validateform(){  
-var name=document.sentMessage.name.value;   
-if (name==null || name==""){  
-  alert("Name can't be blank");  
-  return false;  
-}else if(name.length<6){  
-  alert("Name must be at least 6 characters long.");  
-  return false;  
-  }  
+function validateForm(){  
+ let x = document.forms["myForm"]["name"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
 }   
